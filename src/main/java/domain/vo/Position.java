417ad451ba0bae5dd1cfc446b41d3d725276d47d
sdrs6921 <sdrs6921@naver.com>
2021-09-17 20:19:18 +0900

    feat : equals, hashCode, isLargerThan 테스트 및 기능 구현

--- src/main/java/domain/vo/Position.java
+++ src/main/java/domain/vo/Position.java
@@ -1,5 +1,7 @@
 package domain.vo;
 
+import java.util.Objects;
+
 public class Position {
 
     private final int value;
@@ -14,5 +16,22 @@ public class Position {
 
     public Position increaseBy(final int distanceMovedAtOnce) {
         return new Position(value + distanceMovedAtOnce);
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Position)) return false;
+        final Position position = (Position) o;
+        return value == position.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+
+    public boolean isLargerThan(final Position other) {
+        return value() > other.value();
     }
 }
--- src/test/java/domain/vo/PositionTest.java
+++ src/test/java/domain/vo/PositionTest.java
@@ -2,6 +2,8 @@ package domain.vo;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -26,5 +28,41 @@ class PositionTest {
         int actual = increasedPosition.value();
 
         assertThat(actual).isEqualTo(1);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1,1,true", "1,2,false"})
+    @DisplayName("위치값이 같으면 참, 다르면 거짓을 반환한다")
+    void equals(int value, int otherValue, boolean expected) {
+        Position position = new Position(value);
+        Position other = new Position(otherValue);
+
+        boolean actual = position.equals(other);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1,1,true", "1,2,false"})
+    @DisplayName("위치값이 같으면 참, 다르면 거짓을 반환한다")
+    void hashCode(int value, int otherValue, boolean expected) {
+        Position position = new Position(value);
+        Position other = new Position(otherValue);
+
+        boolean actual = position.hashCode() == other.hashCode();
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"2,1,true", "1,2,false"})
+    @DisplayName("위치값이 같으면 참, 다르면 거짓을 반환한다")
+    void isLargerThan(int value, int otherValue, boolean expected) {
+        Position position = new Position(value);
+        Position other = new Position(otherValue);
+
+        boolean actual = position.isLargerThan(other);
+
+        assertThat(actual).isEqualTo(expected);
     }
 }
