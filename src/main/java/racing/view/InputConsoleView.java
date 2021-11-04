30e257da0feb5b61595c2bb2e28e632cde916232
sdrs6921 <sdrs6921@naver.com>
2021-11-04 20:34:38 +0900

    style : 코드 통일성 추가

--- src/main/java/racing/view/InputConsoleView.java
+++ src/main/java/racing/view/InputConsoleView.java
@@ -18,7 +18,7 @@ public class InputConsoleView implements InputView {
 
     @Override
     public InputCarNameDto inputCarNames() {
-        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
         String nameToken = scanner.nextLine();
         String[] names = nameToken.split(NAME_TOKEN_DELIMITER);
         return new InputCarNameDto(names);
@@ -26,7 +26,7 @@ public class InputConsoleView implements InputView {
 
     @Override
     public InputNumberOfAttemptDto inputNumberOfAttempt() {
-        System.out.println("시도할 회수는 몇회인가요?");
+        out.println("시도할 회수는 몇회인가요?");
         String numberValue = scanner.nextLine();
         return new InputNumberOfAttemptDto(numberValue);
     }
