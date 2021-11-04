1b9f35bc8c81690f53ced4df3e90fdb7a07ef176
sdrs6921 <sdrs6921@naver.com>
2021-11-04 20:34:22 +0900

    refactor : 이름 공백 제거 기능 추가

--- src/main/java/racing/view/InputConsoleView.java
+++ src/main/java/racing/view/InputConsoleView.java
@@ -5,8 +5,11 @@ import racing.dto.InputNumberOfAttemptDto;
 
 import java.util.Scanner;
 
+import static java.lang.System.out;
+
 public class InputConsoleView implements InputView {
 
+    private static final String NAME_TOKEN_DELIMITER = "\\s*,\\s*";
     private final Scanner scanner;
 
     public InputConsoleView(final Scanner scanner) {
@@ -17,7 +20,7 @@ public class InputConsoleView implements InputView {
     public InputCarNameDto inputCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
         String nameToken = scanner.nextLine();
-        String[] names = nameToken.split(",");
+        String[] names = nameToken.split(NAME_TOKEN_DELIMITER);
         return new InputCarNameDto(names);
     }
 
